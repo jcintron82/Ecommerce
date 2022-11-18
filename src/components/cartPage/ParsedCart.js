b16a4986c188396fde8c512718694332dfa29719
jcintron82 <71289948+jcintron82@users.noreply.github.com>
2022-11-18 13:39:37 -0500

    Did basic css styling on the in-cart product wraps to have a barebones layout visually as I complete functionality

--- src/components/cartPage/ParsedCart.js
+++ src/components/cartPage/ParsedCart.js
@@ -6,11 +6,21 @@ export function ParsedCart() {
     return(
         <div>
             {
-                userCartArr.map(product => (
-                    <div className="cartWrap">
-                    <h2 key={product.name}>{product.name}</h2>
-                    <h2 key={product.price}>{product.price}</h2>
-                    </div>
+                userCartArr.map((product, name) => (
+                    <div className="cartWrap" key={product.name}>
+
+                        <div className="userCartProductImages">
+                            <div>{ product.image1 }</div></div>
+                            <div className="cartProductInfoWrap">
+                        <div className="cartProductsDescriptionWrap">                
+                    <h2 className="cartProductsName">{product.name}</h2>
+                    <div>{ product.description }</div></div>
+                    <div className="cartProductsPriceQtyWrap">
+                    <div>{product.price}</div>
+                    <div>qty</div>
+                    </div></div></div>
+
+                  
                 ))
                 
             }
