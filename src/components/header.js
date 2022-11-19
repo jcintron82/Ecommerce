35adcf51be09a8f9c2fae001d104a2ae53d59837
jcintron82 <71289948+jcintron82@users.noreply.github.com>
2022-11-19 18:45:41 -0500

    Added home svg icon to header

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,5 @@
 import {ReactComponent as Cart} from './cart.svg';
+import {ReactComponent as HomeIcon} from './home.svg';
 import { useNavigate } from 'react-router-dom'
 import stock from '../images/stock.avif'
 
@@ -6,13 +7,18 @@ import stock from '../images/stock.avif'
 export function Header() {
 	const navigate = useNavigate();
 
+	const homeClick = () => {
+		navigate('/home')
+	}
+	
+
 	const cartClick = () => {
 		navigate('/usercart')
 	}
 
 	return (
 		<div className='header'>
-		<div> hamburger menu</div>
+		<div><HomeIcon style={{width: '27px'}} onClick={homeClick} /></div>
 		<div> company name/Logo</div>
 		<div> search</div>
 		<div> <Cart style={{width: '27px'}} onClick={cartClick} /> </div>
