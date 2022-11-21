e33a47fc7d1b70e8ae208061294cf6bcd8be5c31
jcintron82 <71289948+jcintron82@users.noreply.github.com>
2022-11-21 17:28:34 -0500

    Added logic to view more images on a product as a scrolling wheel

--- src/components/productdisplays/electronics/Laptop.js
+++ src/components/productdisplays/electronics/Laptop.js
@@ -1,63 +1,70 @@
 import Header from "../../header.js";
 import Button from "../../buttons.js";
-import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { ProductTiles } from "../../productdisplays/producttiles.js";
-import { arr, productFloat } from "../../productdisplays/producttilewrap.js";
-import { userCartArr } from '../../cartPage/ParsedCart.js';
-import macbookFront from '../../../images/electronics/laptop/macbookFront.png'
-import macbookTop from '../../../images/electronics/laptop/macbookTop.png'
-import macbookSide from '../../../images/electronics/laptop/macbookSide.png'
+import { userCartArr } from "../../cartPage/ParsedCart.js";
+import macbookFront from "../../../images/electronics/laptop/macbookFront.png";
+import macbookTop from "../../../images/electronics/laptop/macbookTop.png";
+import macbookSide from "../../../images/electronics/laptop/macbookSide.png";
 
-
-
+let counter = 0;
+const imageGallery = [
+    macbookFront,macbookTop,macbookSide
+]
 
 const laptop = {
-    name: "13.3-inch Apple MacBook Pro",
-    price: 500,
-    description: "A refurbished Macbook Pro 13-Inch. Featuring a 13.3-inch LED-backlit display,",
-    image1: macbookFront,
-    stockQty: 10,
-    orderQty:0,
-    orderTotal:0,
-  }
-
-
+  name: "13.3-inch Apple MacBook Pro",
+  price: 500,
+  description:
+    "A refurbished Macbook Pro 13-Inch. Featuring a 13.3-inch LED-backlit display,",
+  image1: macbookFront,
+  stockQty: 10,
+  orderQty: 0,
+  orderTotal: 0,
+};
 
 export function Laptop() {
-    const [productName, setProductName] = useState("");
-    const [productPrice, setProductPrice] = useState("");
-    const [productDescription, setProductDescription] = useState("");
-    const [productImage1, setProductImage1] = useState("");
-    const [open, setOpen] = useState(false);
- 
-    const handleOpen = () => {
-        setOpen(!open)
+
+  const [productName, setProductName] = useState("");
+  const [productPrice, setProductPrice] = useState("");
+  const [productDescription, setProductDescription] = useState("");
+  const [productImage1, setProductImage1] = useState("");
+  const [open, setOpen] = useState(false);
+//   const [seeMore, setSeeMore] = useState(false);
+
+  const [pic2, setPic2] = useState(imageGallery[counter]);
+  
+
+const nextImageClick = () => {
+    if (counter === 2) {
+        counter = 0
+        setPic2(imageGallery[counter]);
     }
+    else {
+    counter = counter + 1
+    setPic2(imageGallery[counter]); }
+    // setPicThree(macbookTop)
+}
 
+  const handleOpen = () => {
+    setOpen(!open);
+  };
 
+  const addToCartClick = (product) => {
+    if (userCartArr.includes(product)) {
+      console.log("yes 2");
+      product.orderQty++;
+      product.orderTotal = product.price * product.orderQty;
+    } else {
+      product.orderQty++;
+      product.orderTotal = product.price * product.orderQty;
+      userCartArr.push(product);
+    }
+  };
 
-    const addToCartClick = (product) => {
-        if (userCartArr.includes(product)){
-          console.log('yes 2');
-          product.orderQty++;
-          product.orderTotal = (product.price * product.orderQty);
-        }
-        else {
-          product.orderQty++;
-          product.orderTotal = (product.price * product.orderQty);
-          userCartArr.push(product)
-      
-        }
-      
-      
-       
-      }
-
-
-        return (
-            <div>
-            <Header />
+  return (
+    <div>
+      <Header />
 
       <div className="productNameWrap">
         {" "}
@@ -65,47 +72,55 @@ export function Laptop() {
         {laptop.price}
       </div>
       <div className="imagesAndDescFlexWrap">
-        <div className="productImageWrap">
+        <div className="productImageWrap" id="">
           <div className="productWrap">
             {" "}
-            <img className="clickedProductImages" src={macbookFront}></img>
-          </div>
-          <div className="productWrap">
-          {" "}
-            <img className="clickedProductImages" src={ macbookTop }></img> </div>
-
-
-          <div className="productWrap"> 
-          {" "}
-            <img className="clickedProductImages" src={ macbookSide }></img>
+            <img className="clickedProductImages" src={pic2}></img>
           </div>
         </div>
-        <Button text="See More" />
-       <div className="addToCartBtnWrap"> <Button text="Add To Cart" className="addToCartBtn" onClick={e => addToCartClick(laptop)} /></div>
+      
+        <Button 
+            text="See More" 
+            onClick={nextImageClick}
+            className='seeMoreBtn'  />
+            {/* {seeMore ? (
+                <div>GoodJob</div>
+            ) : <div></div>} */}
+
+
+        <div className="addToCartBtnWrap">
+          {" "}
+          <Button
+            text="Add To Cart"
+            className="addToCartBtn"
+            onClick={(e) => addToCartClick(laptop)}
+          />
+        </div>
         <div className="descriptionWrap">
-            
-        <div className="detailsWrap">
-      <Button text='Details' className="collapsibleDetailsBtn" onClick={handleOpen} />
-      {open ? <div className="detailsBoxOne">A refurbished Macbook Pro 13-Inch. Featuring a 13.3-inch LED-backlit display,
-        2560-by-1600 native resolution at 227 pixels per inch.
-        <div>8GB Unified Memory</div>
-        <div>256GB SSD</div>
-        <div>Touch Bar and Touch ID</div>
-        <div>720p FaceTime HD Camera</div>
-      </div> : <div></div>}
-    </div>
-    
-
-
-
-         
-    
+          <div className="detailsWrap">
+            <Button
+              text="Details"
+              className="collapsibleDetailsBtn"
+              onClick={handleOpen}
+            />
+            {open ? (
+              <div className="detailsBoxOne">
+                A refurbished Macbook Pro 13-Inch. Featuring a 13.3-inch
+                LED-backlit display, 2560-by-1600 native resolution at 227
+                pixels per inch.
+                <div>8GB Unified Memory</div>
+                <div>256GB SSD</div>
+                <div>Touch Bar and Touch ID</div>
+                <div>720p FaceTime HD Camera</div>
+              </div>
+            ) : (
+              <div></div>
+            )}
+          </div>
         </div>
       </div>
       <div className="mayAlsoLikeWrap">
-
-
-        <div className="mayAlsoLikeTilesWrap" >
+        <div className="mayAlsoLikeTilesWrap">
           <ProductTiles />
           <ProductTiles />
           <ProductTiles />
@@ -113,8 +128,7 @@ export function Laptop() {
         <div>Footer</div>
       </div>
     </div>
-          
-        )
+  );
 }
 
 export default Laptop;
