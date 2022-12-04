2954f231a70cfc8a4946eff68a02ab45f3545cbf
jcintron82 <71289948+jcintron82@users.noreply.github.com>
2022-12-04 02:22:46 -0500

    still debugging neltify bug

--- src/routing/RouteSwitch.js
+++ src/routing/RouteSwitch.js
@@ -40,7 +40,7 @@ import { Xbox } from "../components/productdisplays/gaming/Xbox.js";
 
 const RouteSwitch = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/home">
       <Routes>
         <Route path="/home" element={<App />} />
         <Route path="/usercart" element={<UserCart />} />
